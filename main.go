956ed6d60cd7b299a75ad169743b4ea33c98077c
Laura Barber <lixxiadev@gmail.com>
2018-11-29 16:03:00 -0500

    Update main.go
    I just do what the workshop tells me.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello darkness my old friend"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
